11c3681d5639ef077a3611522515bad9734d71a8
Pankaj Shah <aaryanshah615@gmail.com>
2023-09-30 22:25:24 +0545

    form added

--- doc_uploader_downloader/app/urls.py
+++ doc_uploader_downloader/app/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     
     path('',views.home,name='home'),
+    path('form/',views.home1,name='form')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- doc_uploader_downloader/app/views.py
+++ doc_uploader_downloader/app/views.py
@@ -5,3 +5,14 @@ from .models import *
 def home(request):
     pdf_obj=pdf.objects.all()
     return render(request,'index.html',{'data':pdf_obj})
+
+
+
+def home1(request):
+    if request.method=='POST':
+        fl=request.FILES['file']
+        name=request.POST['fl']
+        pdf.objects.create(pdf_name=name,pdf=fl)
+        
+    
+    return render(request,'form.html',{'data':''})
